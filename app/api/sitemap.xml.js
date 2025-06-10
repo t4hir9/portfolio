6351bc150e4c3fd8cfdcbe5b86792e298c796120
Khaleel Ibraheem <alhajikhaleel@gmail.com>
2025-06-11 01:00:41 +0530

    fix: set content-type header for sitemap.xml using api

--- app/api/sitemap.xml.js
+++ app/api/sitemap.xml.js
@@ -1,4 +1,5 @@
-<?xml version="1.0" encoding="UTF-8"?>
+export default function handler(req, res) {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://khaleelalhaji.info/</loc>
@@ -12,4 +13,8 @@
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>
-</urlset>
+</urlset>`;
+
+  res.setHeader('Content-Type', 'application/xml');
+  res.status(200).send(sitemap);
+}
