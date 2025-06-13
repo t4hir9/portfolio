6fb55acdc8bab616cacd832fc13240a1d7b57f87
Khaleel Ibraheem <alhajikhaleel@gmail.com>
2025-06-13 20:47:31 +0530

    Add dynamic sitemap route

--- app/sitemap.xml/route.js
+++ app/sitemap.xml/route.js
@@ -1,25 +1,29 @@
-<?xml version="1.0" encoding="UTF-8"?>
+export async function GET() {
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-
   <url>
     <loc>https://khaleelalhaji.info/</loc>
     <lastmod>2025-06-13</lastmod>
     <changefreq>monthly</changefreq>
     <priority>1.0</priority>
   </url>
-
   <url>
     <loc>https://khaleelalhaji.info/projects</loc>
     <lastmod>2025-06-13</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.9</priority>
   </url>
-
   <url>
     <loc>https://khaleelalhaji.info/resume</loc>
     <lastmod>2025-06-13</lastmod>
     <changefreq>yearly</changefreq>
     <priority>0.8</priority>
   </url>
+</urlset>`;
 
-</urlset>
+  return new Response(body, {
+    headers: {
+      'Content-Type': 'application/xml'
+    }
+  });
+}
